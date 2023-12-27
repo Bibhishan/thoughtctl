cd39719784ee4e7b8a620f2e04a0482d2850273d
Bibhisham <87534525+BiBhi@users.noreply.github.com>
2023-12-27 22:15:20 +0530

    date format change

--- Thoughtctl_Task/Thoughtctl_Task/Home/ViewModel/ListViewModel.swift
+++ Thoughtctl_Task/Thoughtctl_Task/Home/ViewModel/ListViewModel.swift
@@ -9,15 +9,15 @@ import Foundation
 
 class ListViewModel {
     
-    private var listModel: [ListModel] = [ListModel(title: "Mahabaleshwar", date: "12-23-2023", postImage: ["1","3","5"]),
-                                          ListModel(title: "Latur", date: "12-23-2023", postImage: ["2","4","6"]),
-                                          ListModel(title: "Lonavala", date: "12-23-2023", postImage: ["5","2","3","5"]),
-                                          ListModel(title: "Panchganiv", date: "12-23-2023", postImage: ["7"]),
-                                          ListModel(title: "Alibaug", date: "12-23-2023", postImage: ["4","3","2"]),
-                                          ListModel(title: "Matheran", date: "12-23-2023", postImage: ["3","1","6"]),
-                                          ListModel(title: "Aurangabad", date: "12-23-2023", postImage: ["6","3","2"]),
-                                          ListModel(title: "Ellora", date: "12-23-2023", postImage: ["5","3","4"]),
-                                          ListModel(title: "Pune", date: "12-23-2023", postImage: ["1","3","7"])
+    private var listModel: [ListModel] = [ListModel(title: "Mahabaleshwar", date: "1/12/23 7:23 AM", postImage: ["1","3","5"]),
+                                          ListModel(title: "Latur", date: "23-11/23 5:23 PM", postImage: ["2","4","6"]),
+                                          ListModel(title: "Lonavala", date: "10/9/23 10:23 AM", postImage: ["5","2","3","5"]),
+                                          ListModel(title: "Panchganiv", date: "26/23/23 10:23 PM", postImage: ["7"]),
+                                          ListModel(title: "Alibaug", date: "2/9/23 10:23 AM", postImage: ["4","3","2"]),
+                                          ListModel(title: "Matheran", date: "8/8/23 9:23 PM", postImage: ["3","1","6"]),
+                                          ListModel(title: "Aurangabad", date: "14/2/23 10:23 AM", postImage: ["6","3","2"]),
+                                          ListModel(title: "Ellora", date: "21/8/23 10:23 PM", postImage: ["5","3","4"]),
+                                          ListModel(title: "Pune", date: "22/6/23 11:23 AM", postImage: ["1","3","7"])
     ]
     
     func getData(index: Int) -> ListModel {
